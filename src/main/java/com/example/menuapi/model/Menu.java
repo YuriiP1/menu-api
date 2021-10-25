ba772aad7d3037e8a3d5556d89c23718642f5965
Yurii Panchyshyn <yurii.panchyshyn@etiya.com>
2021-10-25 12:46:12 +0300

    MA-2 Create Menu entity

---
+++ src/main/java/com/example/menuapi/model/Menu.java
@@ -0,0 +1,23 @@
+package com.example.menuapi.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "menu")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Menu {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "restaurant_id")
+    private List<MenuCategory> menuCategories;}

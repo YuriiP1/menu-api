1b8e154637b5c46d4443f427f5c0be93c34e8075
Yurii Panchyshyn <yurii.panchyshyn@etiya.com>
2021-10-27 14:07:56 +0300

    optional

--- src/main/java/com/example/menuapi/model/dto/RestaurantRequest.java
+++ src/main/java/com/example/menuapi/model/dto/RestaurantRequest.java
@@ -4,23 +4,11 @@ import com.example.menuapi.model.Location;
 import com.example.menuapi.model.Menu;
 import lombok.Data;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-
-
 @Data
 public class RestaurantRequest { ;
 
-    @NotNull(message = "Name shouldn't be null")
-    @NotBlank(message = "Name should have at least 1 symbol")
     private String name;
-
-    @NotNull(message = "Image should be not null")
     private String imageUrl;
-
-    @NotNull(message = "Location is required")
     private Location location;
-
-//    @NotNull(message = "Menu is required")
     private Menu menu;
 }
